2db31ec5be4a8965eb4e19fa3efc8709feb47f0c
Denis-DEV-OPS <80200620+Denis-DEV-OPS@users.noreply.github.com>
2023-12-14 17:50:16 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,3 +49,50 @@ module "eks" {
 resource "aws_ecr_repository" "gdv_ecr_repo" {
   name = "gdv-ecr-repo"
 }
+
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-state"
+
+  tags = {
+    Name = "gdv bucket"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-up-and-running-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
