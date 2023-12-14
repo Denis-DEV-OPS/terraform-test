9536c487bfdb164ee21961702b16a2b9e8357e6f
Denis-DEV-OPS <80200620+Denis-DEV-OPS@users.noreply.github.com>
2023-12-14 13:09:13 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,1 @@
-provider "aws" {
-  region = "us-east-1"
-}
 
-resource "aws_instance" "ec2_instance" {
-  ami           = "ami-0fc5d935ebf8bc3bc"
-  count         = "1"
-  instance_type = "t2.micro"
-  key_name      = "vpc"
-	
-  tags = {
-      Name = "EC2"
-  }
-} 
