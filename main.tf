464ed4dc3fa7d5d6ed2a93975ebe74d231264af3
Denis-DEV-OPS <80200620+Denis-DEV-OPS@users.noreply.github.com>
2023-12-14 11:40:15 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-0fc5d935ebf8bc3bc"
+  count         = "1"
+  instance_type = "t2.micro"
+  key_name      = "vpc"
+	
+  tags = {
+      Name = "TF-Instance"
+  }
+} 
