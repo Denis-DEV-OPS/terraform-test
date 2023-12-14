136112185faa9cd683e12d33f893a7f048f7607c
Denis-DEV-OPS <80200620+Denis-DEV-OPS@users.noreply.github.com>
2023-12-14 13:40:40 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,77 @@
-provider "aws" {
-  region = "us-east-1"
+variable "region" {
+    default = "us-east-1"
 }
+data "aws_availability_zones" "available" {}
+
+locals {
+    cluster_name = "EKS-Cluster"
+}
+module vpc {
+    source = "terraform-aws-modules/vpc/aws"
+    version = "5.4.0"
+    name = "Demo-VPC"
+    cidr = "10.0.0.0/16"
+    azs = data.aws_availability_zones.available.names
+    private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+    public_subnets =  ["10.0.4.0/24", "10.0.5.0/24"]
+
+public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+
+tags = {
+    "Name" = "Demo-VPC"
+}
+public_subnet_tags = {
+    "Name" = "Demo-Public-Subnet"
+}
+private_subnet_tags = {
+    "Name" = "Demo-Private-Subnet"
+}
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.27"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+}
+
