aa963364d117c2bb4950f33fda4dacecf49a1329
Denis-DEV-OPS <80200620+Denis-DEV-OPS@users.noreply.github.com>
2023-12-14 17:04:36 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,51 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+data "aws_availability_zones" "available" {}
+
+locals {
+  cluster_name = "gdv-tf-eks"
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.21.0"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.28"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+  }
+
+ eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t2.micro"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t2.micro"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+}
+
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name = "my-ecr-repo"
+}
