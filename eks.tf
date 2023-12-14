513602eb94d33529fc1b9b5d8bcd032670f487a1
Denis-DEV-OPS <80200620+Denis-DEV-OPS@users.noreply.github.com>
2023-12-14 12:46:58 +0300

    Create eks.tf

---
+++ eks.tf
@@ -0,0 +1,32 @@
+module "eks"{
+    source = "terraform-aws-modules/eks/aws"
+    version = "19.21.0"
+    cluster_name = local.cluster_name
+    cluster_version = "1.28"
+    subnets = module.vpc.private_subnets
+tags = {
+        Name = "Demo-EKS-Cluster"
+    }
+vpc_id = module.vpc.vpc_id
+    workers_group_defaults = {
+        root_volume_type = "gp2"
+    }
+workers_group = [
+        {
+            name = "Worker-Group-1"
+            instance_type = "t2.micro"
+            asg_desired_capacity = 1
+        },
+        {
+            name = "Worker-Group-2"
+            instance_type = "t2.micro"
+            asg_desired_capacity = 1
+        },
+    ]
+}
+data "aws_eks_cluster" "cluster" {
+    name = module.eks.cluster_id
+}
+data "aws_eks_cluster_auth" "cluster" {
+    name = module.eks.cluster_id
+}
